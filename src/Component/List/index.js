aab3ba9d7713226b5044a9f7b2191d4ca09652c0
lixia.zheng <lixia.zheng@bianlifeng.com>
2019-03-19 16:42:34 +0800

    codeing

--- src/Component/List/index.js
+++ src/Component/List/index.js
@@ -1,25 +1,101 @@
 import React, { Component } from 'react';
-import { Icon } from 'antd';
+import { Icon, Rate } from 'antd';
 import PropTypes from 'prop-types';
 import styles from './index.css';
-import ListContent from './listContent';
-
 
 class List extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ListArray: [],
+      activeKey: 0,
+      leftDisable: false,
+      rightDisable: false,
+    };
+  }
+
+  componentDidMount() {
+    const { ListArray } = this.state;
     const { MAP } = this.props;
+    const listBlockNum = parseInt((MAP.length) / 10, 10) + 1;
+    /* eslint-disable-next-line */
+    for (let i = 0; i < listBlockNum; i++) {
+      ListArray.push(MAP.slice(i * 10, (i + 1) * 10));
+    }
+    this.setState({
+      ListArray,
+      leftDisable: ListArray.length === 1,
+      rightDisable: ListArray.length === 1,
+    });
+  }
+
+  back() {
+    const { ListArray } = this.state;
+    let { activeKey } = this.state;
+    if (ListArray.length > 1) {
+      if (activeKey) {
+        activeKey -= 1;
+      } else {
+        activeKey = ListArray.length - 1;
+      }
+    }
+    this.setState({
+      activeKey,
+    });
+  }
+
+  next() {
+    const { ListArray } = this.state;
+    let { activeKey } = this.state;
+    if (ListArray.length > 1) {
+      if (activeKey === ListArray.length - 1) {
+        activeKey = 0;
+      } else {
+        activeKey += 1;
+      }
+    }
+
+    this.setState({
+      activeKey,
+    });
+  }
+
+  render() {
+    const {
+      leftDisable, rightDisable, ListArray, activeKey,
+    } = this.state;
     return (
       <div>
         <div className={styles.filmWrap} />
         <div className={styles.contentbg}>
-          <div className={styles.left}>
-            <Icon type="left" className={styles.icon} />
-          </div>
-          <div className={styles.right}>
-            <Icon type="right" className={styles.icon} />
+          {
+            leftDisable ? null : (
+              /* eslint-disable-next-line */
+              <div className={styles.left} onClick={() => { this.back(); }} onKeyDown={() => {}}>
+                <Icon type="left" className={styles.icon} />
+              </div>
+            )
+          }
+          {
+            rightDisable ? null : (
+            /* eslint-disable-next-line */
+            <div className={styles.right} onClick={() => { this.next(); }} onKeyDown={() => {}}>
+              <Icon type="right" className={styles.icon} />
+            </div>)
+          }
+        </div>
+        <div className={styles.content}>
+          <div className={styles.block}>
+            { Array.isArray(ListArray[activeKey]) && ListArray[activeKey].length
+            && ListArray[activeKey].slice(0, 10).map(val => (
+              <div className={styles.movieWrap}>
+                <img src={`/assets/Image/${val.value}`} alt="val.value" className={styles.pic} />
+                <div>{val.title}</div>
+                <Rate disabled defaultValue={2} className={styles.rate} />
+              </div>
+            ))}
           </div>
         </div>
-        <ListContent MAP={MAP} />
       </div>
     );
   }
--- src/View/Home/index.js
+++ src/View/Home/index.js
@@ -3,12 +3,13 @@ import styles from './index.css';
 
 class Home extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div className={styles.centerText}>
         <div className={styles.welcomeText}>penguin home</div>
         <div className={styles.title}>STARTS WITH YOU</div>
         {/* eslint-disable jsx-a11y/no-static-element-interactions */}
-        <div className={styles.button} onClick={() => {}} onKeyDown={() => {}}>TAKE ACTION</div>
+        <div className={styles.button} onClick={() => { history.push('/film'); }} onKeyDown={() => {}}>TAKE ACTION</div>
       </div>
     );
   }
